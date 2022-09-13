c2ce981155381205565417aa4f6c0494279532c9
256po <paul.bewayo4@gmail.com>
2022-09-13 10:20:09 -0700

    v

---
+++ 0-putchar.c
@@ -0,0 +1,17 @@
+#include "paul.h"
+
+/**
+ * main - Entry point
+ *
+ *  Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	_putchar('P');
+	_putchar('a');
+	_putchar('u');
+	_putchar('l');
+	_putchar('\n');
+	return (0);
+}
