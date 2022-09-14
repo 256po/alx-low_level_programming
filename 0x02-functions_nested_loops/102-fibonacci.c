cfdc54ed7037ac8f0bab14f1ba44ed366f510b0e
256po <paul.bewayo4@gmail.com>
2022-09-13 18:24:24 -0700

    y

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+ #include <stdio.h>
 /**
  * main - main function
- * 
+ *
  * Return: nothing
- */
+ */ 
 int main(void)
 {
 	int counter = 2;
 	long int a = 1;
-	long int b = a + 1
+	long int b = a + 1;
 	long int c = a + b;
 
 	printf("%ld, %ld, ", a, b);
