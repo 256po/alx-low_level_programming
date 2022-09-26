d745fb4a020370473bf2f27598006fc2db0f9a84
256po <paul.bewayo4@gmail.com>
2022-09-26 11:18:04 -0700

    v

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * memcpy - a function that copies memory area
+ * _memcpy - a function that copies memory area
  * @dest: memory where is stored
  * @src: memory where is copied
  * @n: number of bytes
